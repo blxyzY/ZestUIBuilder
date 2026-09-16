677dd3e21f234efa6d4879e2835702818f6b7a64
lvxy_10 <89435378+blxyzY@users.noreply.github.com>
2026-09-16 22:40:30 +0700

    Remove unused variables and options in extract_fw.sh

--- extract_fw.sh
+++ extract_fw.sh
@@ -10,7 +10,6 @@ FORCE=false
 FIRMWARES=()
 MODEL=""
 CSC=""
-LATEST_FIRMWARE=""
 DOWNLOADED_FIRMWARE=""
 BL_TAR=""
 AP_TAR=""
@@ -170,56 +169,24 @@ GET_IMAGE_FILE_SYSTEM()
 
 PREPARE_SCRIPT()
 {
-    local EXTRA_FIRMWARES=()
-    local IGNORE_SOURCE=false
-    local IGNORE_TARGET=false
-
     while [ "$#" != 0 ]; do
         if [[ "$1" == "--force" ]] || [[ "$1" == "-f" ]]; then
             FORCE=true
-        elif [[ "$1" == "--ignore-source" ]]; then
-            IGNORE_SOURCE=true
-        elif [[ "$1" == "--ignore-target" ]]; then
-            IGNORE_TARGET=true
         elif [[ "$1" == "-"* ]]; then
             LOGE "Unknown option: $1"
             PRINT_USAGE
             exit 1
         else
-            EXTRA_FIRMWARES+=("$1")
+            FIRMWARES+=("$1")
         fi
 
         shift
     done
-
-    if ! $IGNORE_SOURCE; then
-        _CHECK_NON_EMPTY_PARAM "SOURCE_FIRMWARE" "$SOURCE_FIRMWARE" || exit 1
-        FIRMWARES+=("$SOURCE_FIRMWARE")
-        IFS=':' read -r -a SOURCE_EXTRA_FIRMWARES <<< "$SOURCE_EXTRA_FIRMWARES"
-        if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
-            FIRMWARES+=("${SOURCE_EXTRA_FIRMWARES[@]}")
-        fi
-    fi
-
-    if ! $IGNORE_TARGET; then
-        _CHECK_NON_EMPTY_PARAM "TARGET_FIRMWARE" "$TARGET_FIRMWARE" || exit 1
-        FIRMWARES+=("$TARGET_FIRMWARE")
-        IFS=':' read -r -a TARGET_EXTRA_FIRMWARES <<< "$TARGET_EXTRA_FIRMWARES"
-        if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
-            FIRMWARES+=("${TARGET_EXTRA_FIRMWARES[@]}")
-        fi
-    fi
-
-    if [ "${#EXTRA_FIRMWARES[@]}" -ge 1 ]; then
-        FIRMWARES+=("${EXTRA_FIRMWARES[@]}")
-    fi
 }
 
 PRINT_USAGE()
 {
     echo "Usage: extract_fw [options] <firmware>" >&2
-    echo " --ignore-source : Skip parsing source firmware flags" >&2
-    echo " --ignore-target : Skip parsing target firmware flags" >&2
     echo " -f, --force : Force firmware extract" >&2
 }
 
@@ -349,33 +316,14 @@ PREPARE_SCRIPT "$@"
 for i in "${FIRMWARES[@]}"; do
     PARSE_FIRMWARE_STRING "$i" || exit 1
 
-    LATEST_FIRMWARE="$(GET_LATEST_FIRMWARE "$MODEL" "$CSC")"
-    if [ ! "$LATEST_FIRMWARE" ]; then
-        LOGE "Latest available firmware could not be fetched"
-        exit 1
-    fi
-
     LOG_STEP_IN "- Processing $MODEL firmware with $CSC CSC"
-    LOG "- Downloaded firmware: $(cat "$ODIN_DIR/${MODEL}_${CSC}/.downloaded" 2> /dev/null)"
-    LOG "- Extracted firmware: $(cat "$FW_DIR/${MODEL}_${CSC}/.extracted" 2> /dev/null)"
-    LOG "- Latest available firmware: $LATEST_FIRMWARE"
 
     LOG_STEP_IN
 
     if ! $FORCE; then
         # Skip if firmware has been extracted
         if [ -f "$FW_DIR/${MODEL}_${CSC}/.extracted" ]; then
-            if ! COMPARE_SEC_BUILD_VERSION "$(cat "$FW_DIR/${MODEL}_${CSC}/.extracted")" "$LATEST_FIRMWARE"; then
-                if [ -f "$ODIN_DIR/${MODEL}_${CSC}/.downloaded" ] && \
-                        ! COMPARE_SEC_BUILD_VERSION "$(cat "$FW_DIR/${MODEL}_${CSC}/.extracted")" "$(cat "$ODIN_DIR/${MODEL}_${CSC}/.downloaded")"; then
-                    LOG "\033[0;33m! A newer firmware has been downloaded, use --force flag if you want to overwrite it\033[0m"
-                else
-                    LOG "\033[0;33m! A newer firmware is available for download\033[0m"
-                fi
-            else
-                LOG "\033[0;33m! This firmware has already been extracted\033[0m"
-            fi
-
+            LOG "\033[0;33m! This firmware has already been extracted\033[0m"
             LOG_STEP_OUT; LOG_STEP_OUT
             continue
         fi
